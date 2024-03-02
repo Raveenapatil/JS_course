80063475f49f76d573af21a1c0f515893afbef3a
Raveenapatil <45459483+Raveenapatil@users.noreply.github.com>
2024-03-02 13:11:03 +0530

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,19 +7,109 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+  openingHours:{
+    thu:{
+      open:12,
+      close:22,
     },
-    fri: {
-      open: 11,
-      close: 23,
+    fri:{
+      open:11,
+      close:23,
     },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+    sat:{
+      open:0,//open 24 hrs
+      close:24,
     },
   },
+  order:function(starterIndex,mainIndex){
+      return[this.starterMenu[starterIndex],this.mainMenu[mainIndex]];  
+  },
+  orderDelivery: function({starterIndex,mainIndex,address,time}){
+    console.log(`order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} at ${address} by ${time}`);
+
+
+  },
+   
 };
+
+// destructing arrays---------------------------------//
+const arr = [2,3,4];
+const ax = arr[0];
+const ba = arr[1];
+const ca = arr[2];
+
+const [x,y,z]= arr;
+console.log(x,y,z);
+
+let [main, ,secondary] = restaurant.categories;//restaurant.categories[0,,2]
+console.log(main, secondary);
+
+//switch the two variables without using temp variable
+[main,secondary]=[secondary,main];
+console.log(main, secondary);
+
+
+//recieving 2 values from function
+const [starter, mainCourse] = restaurant.order(3,2);
+console.log(starter,mainCourse);
+
+//nested array
+const arra = [2, 4, [5,6]];
+console.log(arra);
+const [i, ,j]=arra;
+console.log(i,j);
+const [k,,[o,p]]=arra;
+console.log(k,o,p);
+
+//default values
+const [q=1,r=1,s=1]=[,9,12]
+console.log(q,r,s);
+
+//---------------destructing objects-------------//  
+const {name,openingHours,categories}=restaurant;
+console.log(name,openingHours,categories);
+
+//assigning new name to object
+const{
+  name:hotelname,
+  openingHours:time,
+  categories:tags,
+}=restaurant;
+console.log(hotelname,time,tags);
+
+//default values'
+const{
+  menu=[],starterMenu:starters=[]
+}=restaurant;
+console.log(menu,starters);
+
+// mutating variables(changing)
+let a =111;
+let b =93;
+const obj ={
+  a:23,b:22,c:7
+};
+({a,b}=obj);
+console.log(a,b);
+
+const {fri: { open, close },} = openingHours;
+console.log(open, close);
+
+const { open:oe, close:c } = restaurant.openingHours.fri;
+ 
+console.log(oe, c);
+
+restaurant.orderDelivery({
+  time:"12:20",
+  address:"ghatkopar",
+  starterIndex: 2,
+  mainIndex:1,
+});
+
+//---------------spread operators-------------//  
+const ass =[1,2,3];
+const badarr= [ass[0],ass[1],ass[2],4,5,6];
+console.log(badarr);
+
+const goodarr = [...ass,4,5,7];
+console.log(goodarr);console.log(...goodarr);
